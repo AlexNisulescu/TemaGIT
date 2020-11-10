8cf08763e88154b973bab6d282d34c4dff538e9c
Alex-Nisulescu <alex.nisulescu1998@gmail.com>
2020-11-10 18:14:15 +0200

    Adaugare functionalitati clasa utilizator
    Am adaugat clasei utilizator un nume, anul nasterii si un array list cu produse deja cumparate.
    Am mai adaugat pentru fiecare dintre acestea gettere si settere.
    Am adaugat si functia pentru crearea clasei.

--- src/MagazinVirtual/utilizator.java
+++ src/MagazinVirtual/utilizator.java
@@ -2,11 +2,43 @@ package MagazinVirtual;
 
 import java.util.ArrayList;
 
+/**
+ * Aceasta este clasa pentru utilizator
+ * @author Alex Nisulescu
+ */
+
 public class utilizator {
     private String NumeUtilizator;
-    private ArrayList<produse> carti;
+    private int anNastere;
+    private ArrayList<produse> produse;
 
-    public utilizator(String nume){
+    public utilizator(String nume, int an){
+
         NumeUtilizator=nume;
+        anNastere=an;
+    }
+
+    /**
+     *
+     * @return Numele Utilizatorului
+     */
+    public String getNumeUtilizator() {
+        return NumeUtilizator;
+    }
+
+    /**
+     *
+     * @return arraylist produse
+     */
+    public ArrayList<produse> getProduse() {
+        return produse;
+    }
+
+    public void setAnNastere(int anNastere) {
+        this.anNastere = anNastere;
+    }
+
+    public addProdus(produse p){
+        this.produse.add(p);
     }
 }
