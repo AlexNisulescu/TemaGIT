5f0b37a28f50569acc44423e09ea6680d0bb495a
Alex-Nisulescu <alex.nisulescu1998@gmail.com>
2020-11-10 18:35:53 +0200

    Adaugare optiune cumparare Adaugare metoda de cumparare la clasa utilizator

--- src/MagazinVirtual/utilizator.java
+++ src/MagazinVirtual/utilizator.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 public class utilizator {
     private String NumeUtilizator;
     private int anNastere;
-    private ArrayList<produse> produse;
+    private ArrayList<produse> prod;
     int bani;
 
     public utilizator(String nume, int an){
@@ -20,6 +20,18 @@ public class utilizator {
         this.bani=0;
     }
 
+    public cumparareProdus(prod p)
+    {
+        if (this.bani>=p.getPret())
+        {
+            this.bani-=p.getPret();
+            this.prod.add(p);
+        }
+        else{
+            System.out.println("Utilizatorul nu are suficienti bani");
+        }
+    }
+
     /**
      *
      * @return Numele Utilizatorului
@@ -27,21 +39,20 @@ public class utilizator {
     public String getNumeUtilizator() {
         return NumeUtilizator;
     }
-
     /**
      *
      * @return arraylist produse
      */
-    public ArrayList<produse> getProduse() {
-        return produse;
+    public ArrayList<prod> getProduse() {
+        return prod;
     }
 
     public void setAnNastere(int anNastere) {
         this.anNastere = anNastere;
     }
 
-    public addProdus(produse p){
-        this.produse.add(p);
+    public addProdus(prod p){
+        this.prod.add(p);
     }
 
     public void AdaugareBani(int bani) {
