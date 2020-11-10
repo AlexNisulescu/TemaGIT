537b7c76660943cb5470cb0c5f5a655fd218bbba
Alex-Nisulescu <alex.nisulescu1998@gmail.com>
2020-11-10 18:20:00 +0200

    Adaugare functionalitati clasa produse
    Am adaugat clasei produse numele, producatorul si pretul.
    Am creat constructorul.
    Am creat getter si setter pentru fiecare dintre ele.

--- src/MagazinVirtual/produse.java
+++ src/MagazinVirtual/produse.java
@@ -1,5 +1,38 @@
 package MagazinVirtual;
 
 public class produse {
+    private String numeProdus;
+    private  String Producator;
+    private int Pret;
+
+    public produse(String numeProdus ,String producator, int pret) {
+        this.numeProdus = numeProdus;
+        this.Producator=producator;
+        this.Pret=pret;
+    }
+
+    public String getNumeProdus() {
+        return numeProdus;
+    }
+
+    public void setNumeProdus(String numeProdus) {
+        this.numeProdus = numeProdus;
+    }
+
+    public String getProducator() {
+        return Producator;
+    }
+
+    public void setProducator(String producator) {
+        Producator = producator;
+    }
+
+    public int getPret() {
+        return Pret;
+    }
+
+    public void setPret(int pret) {
+        Pret = pret;
+    }
 
 }
