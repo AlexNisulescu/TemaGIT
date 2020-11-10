ba748d9602af81217a0bd6a7304586940596d46e
0x435446 <0x435446@gmail.com>
2020-11-10 18:33:42 +0200

    Am adaugat clasa Administrator
    Clasa Administrator are toate functionalitatile implementate
    
    Signed-off-by: 0x435446 <0x435446@gmail.com>

--- src/MagazinVirtual/Administrator.java
+++ src/MagazinVirtual/Administrator.java
@@ -1,9 +1,35 @@
 package MagazinVirtual;
 
+import java.util.ArrayList;
+
 public class Administrator {
 
+    private ArrayList <Magazin> Magazine;
     private String NumeAdmin;
+
     public Administrator(String nume){
         NumeAdmin=nume;
     }
+
+    public void setNumeAdmin(String numeAdmin) {
+        NumeAdmin = numeAdmin;
+    }
+
+    public String getNumeAdmin() {
+        return NumeAdmin;
+    }
+
+    public void addMagazin(Magazin M){
+        this.Magazine.add(M);
+    }
+
+    public Magazin getMagazin(int x){
+        return this.Magazine.get(x);
+    }
+
+    public int getData(int x)
+    {
+        return Magazine.get(x).getSold();
+    }
+
 }
