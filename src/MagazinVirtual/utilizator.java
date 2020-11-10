6b490512e2c77f1264117fa7bd7795ac46069c6c
Alex-Nisulescu <alex.nisulescu1998@gmail.com>
2020-11-10 18:01:22 +0200

    Adaugare clase Am creat clasele pentru utilizatori si pentru produse

---
+++ src/MagazinVirtual/utilizator.java
@@ -0,0 +1,12 @@
+package MagazinVirtual;
+
+import java.util.ArrayList;
+
+public class utilizator {
+    private String NumeUtilizator;
+    private ArrayList<produse> carti;
+
+    public utilizator(String nume){
+        NumeUtilizator=nume;
+    }
+}
